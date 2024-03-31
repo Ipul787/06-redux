d4544701a287a363c47945afeca5907008b231c0
Saefulloh Fatah <saefulloh0707@gmail.com>
2024-03-31 17:35:30 +0700

    First commit

---
+++ redux/auth/authSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const initialState = {
+    isLogin: false,
+};
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setLogin(state, action){
+            if (action && action.payload && typeof action.payload.isLogin !== 'undefined') {
+                state.isLogin = action.payload.isLogin;
+            }
+        },
+    },
+});
+
